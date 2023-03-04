a67dbbeda67a5b226c8b031a21deac32e9a169d2
Pablo Calero <pjcalerog@gmail.com>
2023-03-04 15:51:57 +0100

    Actualizacion Index

--- index.php
+++ index.php
@@ -9,16 +9,16 @@
     <body>
         <h1>SERVIDOR STREAMING</h1>
         <div id="chat">
+            <div id="comments"></div>
+
+            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+            <script src="live-comments.js"></script>
+
             <form id="comment-form">
                 <input type="text" name="name" placeholder="Nombre"><br>
                 <textarea name="comment" placeholder="Comentario"></textarea><br>
                 <input type="submit" value="Enviar comentario">
             </form>
-
-            <div id="comments"></div>
-
-            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-            <script src="live-comments.js"></script>
         </div>
 
         <pre>
--- procesaRegistro.php
+++ procesaRegistro.php
@@ -14,29 +14,30 @@ if ($conn->connect_error) {
 // Procesa el formulario de registro
 if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["passwordC"])) {
 
-    $sql = "SELECT * FROM users WHERE username='$username'";
+    $sql = "SELECT * FROM users WHERE username='$_POST[username]'";
     $result = $conn->query($sql);
     if ($result->num_rows == 1) {
         session_start();
         $_SESSION['usuE']=1;
         header('Location: ./registro.php');
-    }
-
-    if ( $_POST["password"] == $_POST["passwordC"] ) {
-        $username = $_POST["username"];
-        $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
-
-        // Inserta el usuario en la base de datos
-        $sql = "INSERT INTO users (username, password)
-                VALUES ('$username', '$password')";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "Usuario registrado exitosamente";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
     } else {
-        echo "Comprueba que has escrito la misma contraseña las dos veces.";
+
+        if ( $_POST["password"] == $_POST["passwordC"] ) {
+            $username = $_POST["username"];
+            $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+            // Inserta el usuario en la base de datos
+            $sql = "INSERT INTO users (username, password)
+                    VALUES ('$username', '$password')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "Usuario registrado exitosamente";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        } else {
+            echo "Comprueba que has escrito la misma contraseña las dos veces.";
+        }
     }
 }
 
